eb153c7e6b0a38d156cb471d99bea6fbc2ab67fb
Cheolu.B <dev.cheolubak@gmail.com>
2022-12-01 22:59:06 +0900

    database module

---
+++ src/database.config.ts
@@ -0,0 +1,16 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { registerAs } from '@nestjs/config';
+
+export default registerAs(
+  'database.config',
+  (): TypeOrmModuleOptions => ({
+    type: 'mariadb',
+    host: process.env.DATABASE_HOST,
+    port: +(process.env.DATABASE_PORT || 3306),
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    entities: [__dirname + './**/*.entity{.ts,.js}'],
+    synchronize: true,
+  }),
+);
